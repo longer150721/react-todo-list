3425c7b2f9f5acf1b5f62bf24054f9e2e4b7f44a
VULCAN\longer‘s <275868482@qq.com>
2017-11-20 01:59:48 +0800

    第二次提交

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,66 +4,73 @@ require('styles/App.css');
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+let memory = new Array();
+let now_page = 0;
+let myDate = new Date();
+
+
 let AppComponent = React.createClass({
   Search: function(num) {
+    if(num == '-1'){
+      now_page = 0;
+      ReactDOM.findDOMNode(this.refs['head']).innerHTML = '<span class="headline-main">Lanciy\'s </span>to do list';
+      ReactDOM.findDOMNode(this.refs['part1']).innerHTML = 'This is a stupid app';
+      ReactDOM.findDOMNode(this.refs['part2']).innerHTML = 'You can click the bottom left';
+      ReactDOM.findDOMNode(this.refs['detail']).innerHTML = 'Or add details in input box here';
+      if (memory[now_page]){ReactDOM.findDOMNode(this.refs['detail']).innerHTML = memory[now_page];}
+      else {ReactDOM.findDOMNode(this.refs['detail']).innerHTML = 'No detail'}
+    }
     if(num == '1'){
-      let headDOM = ReactDOM.findDOMNode(this.refs['head']);
-      let part1DOM = ReactDOM.findDOMNode(this.refs['part1']);
-      let part2DOM = ReactDOM.findDOMNode(this.refs['part2']);
-      headDOM.innerHTML = '<span class="headline-main">The lib one has been done</span>';
-      part1DOM.innerHTML = 'Eventual score : 75';
-      part2DOM.innerHTML = 'Max score : 75';
+      now_page = 1;
+      ReactDOM.findDOMNode(this.refs['head']).innerHTML = '<span class="headline-main">The lib one has been done</span>';
+      ReactDOM.findDOMNode(this.refs['part1']).innerHTML= 'Eventual score : 75';
+      ReactDOM.findDOMNode(this.refs['part2']).innerHTML = 'Max score : 75';
+      if (memory[now_page]){ReactDOM.findDOMNode(this.refs['detail']).innerHTML = memory[now_page];}
+      else {ReactDOM.findDOMNode(this.refs['detail']).innerHTML = 'No detail'}
     }
     if(num == '2'){
-      let headDOM = ReactDOM.findDOMNode(this.refs['head']);
-      let part1DOM = ReactDOM.findDOMNode(this.refs['part1']);
-      let part2DOM = ReactDOM.findDOMNode(this.refs['part2']);
-      headDOM.innerHTML = '<span class="headline-main">The lib two has been done</span>';
-      part1DOM.innerHTML = 'Phases Defused : 7';
-      part2DOM.innerHTML = 'Explosions : 3';
+      now_page = 2;
+      ReactDOM.findDOMNode(this.refs['head']).innerHTML = '<span class="headline-main">The lib two has been done</span>';
+      ReactDOM.findDOMNode(this.refs['part1']).innerHTML = 'Phases Defused : 7';
+      ReactDOM.findDOMNode(this.refs['part2']).innerHTML = 'Explosions : 3';
+      if (memory[now_page]){ReactDOM.findDOMNode(this.refs['detail']).innerHTML = memory[now_page];}
+      else {ReactDOM.findDOMNode(this.refs['detail']).innerHTML = 'No detail'}
     }
     if(num == '3'){
-      let headDOM = ReactDOM.findDOMNode(this.refs['head']);
-      let part1DOM = ReactDOM.findDOMNode(this.refs['part1']);
-      let part2DOM = ReactDOM.findDOMNode(this.refs['part2']);
-      headDOM.innerHTML = '<span class="headline-main">The lib three has been done</span>';
-      part1DOM.innerHTML = 'Eventual score : 60';
-      part2DOM.innerHTML = 'Max score : 60';
+      now_page = 3;
+      ReactDOM.findDOMNode(this.refs['head']).innerHTML = '<span class="headline-main">The lib three has been done</span>';
+      ReactDOM.findDOMNode(this.refs['part1']).innerHTML = 'Eventual score : 60';
+      ReactDOM.findDOMNode(this.refs['part2']).innerHTML = 'Max score : 60';
+      if (memory[now_page]){ReactDOM.findDOMNode(this.refs['detail']).innerHTML = memory[now_page];}
+      else {ReactDOM.findDOMNode(this.refs['detail']).innerHTML = 'No detail'}
     }
     if(num == '4'){
-      let headDOM = ReactDOM.findDOMNode(this.refs['head']);
-      let part1DOM = ReactDOM.findDOMNode(this.refs['part1']);
-      let part2DOM = ReactDOM.findDOMNode(this.refs['part2']);
-      headDOM.innerHTML = '<span class="headline-main">The lib four is waiting for being done</span>';
-      part1DOM.innerHTML = 'deadline : 11/27/2017';
-      part2DOM.innerHTML = 'days left : 6';
+      now_page = 4;
+      let dayleft = 27 - myDate.getDate();
+      ReactDOM.findDOMNode(this.refs['head']).innerHTML = '<span class="headline-main">The lib four is waiting for being done</span>';
+      ReactDOM.findDOMNode(this.refs['part1']).innerHTML= 'Deadline : 11/27/2017';
+      if (dayleft > 0){ReactDOM.findDOMNode(this.refs['part2']).innerHTML = 'Days left : '+ dayleft;}
+      else {ReactDOM.findDOMNode(this.refs['part2']).innerHTML = 'Beyond the deadline'}
+      if (memory[now_page]){ReactDOM.findDOMNode(this.refs['detail']).innerHTML = memory[now_page];}
+      else {ReactDOM.findDOMNode(this.refs['detail']).innerHTML = 'No detail'}
     }
     if(num == '5'){
-      let headDOM = ReactDOM.findDOMNode(this.refs['head']);
-      let part1DOM = ReactDOM.findDOMNode(this.refs['part1']);
-      let part2DOM = ReactDOM.findDOMNode(this.refs['part2']);
-      headDOM.innerHTML = '<span class="headline-main">The lib five has not been shown</span>';
-      part1DOM.innerHTML = 'may be shown on 11/30/2017';
-      part2DOM.innerHTML = 'ddl may on 12/8/2017';
+      now_page = 5;
+      ReactDOM.findDOMNode(this.refs['head']).innerHTML = '<span class="headline-main">The lib five has not been shown</span>';
+      ReactDOM.findDOMNode(this.refs['part1']).innerHTML = 'May be shown on 11/30/2017';
+      ReactDOM.findDOMNode(this.refs['part2']).innerHTML = 'Ddl may on 12/8/2017';
+      if (memory[now_page]){ReactDOM.findDOMNode(this.refs['detail']).innerHTML = memory[now_page];}
+      else {ReactDOM.findDOMNode(this.refs['detail']).innerHTML = 'No detail'}
     }
     if(num == '6'){
-      let headDOM = ReactDOM.findDOMNode(this.refs['head']);
-      let part1DOM = ReactDOM.findDOMNode(this.refs['part1']);
-      let part2DOM = ReactDOM.findDOMNode(this.refs['part2']);
-      headDOM.innerHTML = '<span class="headline-main">The lib six has not been shown</span>';
-      part1DOM.innerHTML = 'may be shown on 12/14/2017';
-      part2DOM.innerHTML = 'ddl may on 12/25/2017';
+      now_page = 6;
+      ReactDOM.findDOMNode(this.refs['head']).innerHTML = '<span class="headline-main">The lib six has not been shown</span>';
+      ReactDOM.findDOMNode(this.refs['part1']).innerHTML = 'May be shown on 12/14/2017';
+      ReactDOM.findDOMNode(this.refs['part2']).innerHTML = 'Ddl may on 12/25/2017';
+      if (memory[now_page]){ReactDOM.findDOMNode(this.refs['detail']).innerHTML = memory[now_page];}
+      else {ReactDOM.findDOMNode(this.refs['detail']).innerHTML = 'No detail'}
     }
-    if(num == '-1'){
-      let headDOM = ReactDOM.findDOMNode(this.refs['head']);
-      let part1DOM = ReactDOM.findDOMNode(this.refs['part1']);
-      let part2DOM = ReactDOM.findDOMNode(this.refs['part2']);
-      let detailDOM = ReactDOM.findDOMNode(this.refs['detail']);
-      headDOM.innerHTML = '<span class="headline-main">Lanciy\'s </span>to do list';
-      part1DOM.innerHTML = '这是一个很傻逼的app';
-      part2DOM.innerHTML = '请点击左边的按钮';
-      detailDOM.innerHTML = '在下面的输入框添加备注';
-    }
+
   },
 
   Change:function(ref){
@@ -81,9 +88,11 @@ let AppComponent = React.createClass({
   },
 
   submit:function(){
-    let inputDOM = ReactDOM.findDOMNode(this.refs['submit']);
+    let input = ReactDOM.findDOMNode(this.refs['input']).value;
     let detailDOM = ReactDOM.findDOMNode(this.refs['detail']);
-    detailDOM.innerHTML += inputDOM.value;
+    detailDOM.innerHTML = input;
+    memory[now_page] = input;
+    ReactDOM.findDOMNode(this.refs['input']).value = '';
   },
 
   handleChange: function(event) {
@@ -113,18 +122,40 @@ let AppComponent = React.createClass({
         </div>
         <div className="main-content">
           <h1 className="headline" ref="head"><span className="headline-main">Lanciy's </span>to do list</h1>
-          <p ref="part1">这是一个很傻逼的app</p>
-          <p ref="part2">请点击左边的按钮</p>
-          <p ref="detail"/>
+          <p ref="part1">This is a stupid app</p>
+          <p ref="part2">You can click the bottom left</p>
+          <p ref="detail">Or add details in input box here</p>
           <br/><br/><br/>
-          <input ref="input" type="text" onChange={this.handleChange}/>
+          <input ref="input" type="text" maxLength={25} onChange={this.handleChange}/>
           <br/><br/>
-          <a href="#" ref="submit" onClick={this.submit}>点击此处添加备注</a>
+          <a href="#" ref="submit" onClick={this.submit}>Click here to add details</a>
+        </div>
+          <div id="root" className="corner">
         </div>
       </div>
     );
   }
 });
+
+function ticking(){
+  let seperator1 = '-';
+  let seperator2 = ':';
+  let month = new Date().getMonth() + 1;
+  let strDate = new Date().getDate();
+  const element = (
+    <div>
+      <h1>{'Time : ' + new Date().getFullYear() + seperator1 + month + seperator1 + strDate
+      + ' ' + new Date().getHours() + seperator2 + new Date().getMinutes()
+      + seperator2 + new Date().getSeconds()}</h1>
+    </div>
+  );
+  ReactDOM.render(
+    element,
+    document.getElementById('root')
+  );
+}
+
+setInterval(ticking, 1000);
 
 AppComponent.defaultProps = {
 };
